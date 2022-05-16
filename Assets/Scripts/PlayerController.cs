160fb8abef5ff634dce046503966fad5685e19cf
Atik Mandhat <atikmandhat86@gmail.com>
2022-05-16 11:50:18 +0530

    animals movement , instatiate  animals, Spawn Manager

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed=10.0f;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    
     void Update()
     {
 
@@ -32,6 +35,13 @@ public class PlayerController : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);   
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch projectile from player
+            // Instantiate create a copy of objects
+            Instantiate(projectilePrefab, transform.position , projectilePrefab.transform.rotation);
+        }
         
     }
 }
